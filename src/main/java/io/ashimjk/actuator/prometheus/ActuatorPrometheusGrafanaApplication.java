26bfd1aaa0e504451620ad683869c782ab0454be
Ashim Jung Khadka <ashim.khadka@clusus.com>
2020-10-04 16:46:12 +0545

    feat: initialize

---
+++ src/main/java/io/ashimjk/actuator/prometheus/ActuatorPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package io.ashimjk.actuator.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorPrometheusGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorPrometheusGrafanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ashimjk/actuator/prometheus/ActuatorPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ashimjk.actuator.prometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorPrometheusGrafanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
