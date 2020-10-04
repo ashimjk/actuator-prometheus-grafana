fa6e3a4c0b290a231c93331c45e4f2dad94e2536
Ashim Jung Khadka <ashim.khadka@clusus.com>
2020-10-04 18:19:02 +0545

    feat: enable httptrace and auditevents

--- src/main/java/io/ashimjk/actuator/prometheus/ActuatorPrometheusGrafanaApplication.java
+++ src/main/java/io/ashimjk/actuator/prometheus/ActuatorPrometheusGrafanaApplication.java
@@ -1,10 +1,25 @@
 package io.ashimjk.actuator.prometheus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorPrometheusGrafanaApplication {
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
+    @Bean
+    public AuditEventRepository auditEventRepository() {
+        return new InMemoryAuditEventRepository();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorPrometheusGrafanaApplication.class, args);
